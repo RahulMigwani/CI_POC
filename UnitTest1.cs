8e90a1c2d971702bf973076c251c998cf3437d9e
RahulMigwani <rahul.migwani@accenture.com>
2016-06-12 20:16:29 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CI_Test.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+
+            throw new Exception();
+        }
+    }
+}
